9fb4d1e04d2f7993429eae70e2996cc8e03eb933
dhoniaridho <dhoniaridho@gmail.com>
2022-09-24 01:10:48 +0700

    feat: env variable

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export const appConfig = {
+  api: {
+    base_url: import.meta.env.VITE_API_BASE_URL,
+  },
+}
--- src/modules/http.ts
+++ src/modules/http.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import type { App } from 'vue'
+import { appConfig } from '~/config'
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: appConfig.api.base_url,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
@@ -11,6 +12,8 @@ const http = axios.create({
 const install = (app: App) => {
   app.provide('http', http)
 }
+
+console.log(appConfig)
 
 export { http }
 export default install
